9d83fd87fc8b9ba2d315a0f49031968483b0c152
magneum <krakinzkon@gmail.com>
2023-01-15 04:49:32 -0800

    krytek-yts

--- routes/anime.js
+++ routes/anime.js
@@ -26,7 +26,6 @@ const { v4: uuidv4 } = require("uuid");
 var malScraper = require("mal-scraper");
 var animation = require("anime-actions");
 var { fromBuffer } = require("file-type");
-var YouTube_Scrape = require("krytek-yts");
 var { YTAudio, YTVideo } = require("../src/y2mate");
 var YouTube_Downloader = require("../src/YouTube_Downloader");
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
